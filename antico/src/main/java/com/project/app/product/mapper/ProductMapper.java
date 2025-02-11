7bb2ac80e05a37bb0bfe0d7e58916e5b47dc5369
HalftheWorldAway <qhdb18@naver.com>
2025-02-11 14:31:51 +0900

    [feat] 상품 등록 view 페이지 생성
    1. 상품 등록 view 페이지 생성
    2. header 페이지 판매하기 버튼 링크 수정

---
+++ antico/src/main/java/com/project/app/product/mapper/ProductMapper.java
@@ -0,0 +1,8 @@
+package com.project.app.product.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductMapper {
+
+}
---
+++ antico/src/main/java/com/project/app/product/service/ProductService.java
@@ -0,0 +1,5 @@
+package com.project.app.product.service;
+
+public interface ProductService {
+
+}
