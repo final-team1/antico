dbda2b0e50e7658da2c36057d1c9e269ade9773e
wogurwogur <153715337+wogurwogur@users.noreply.github.com>
2025-02-13 17:54:03 +0900

    feat:커스텀유저Detail 작성
    1. 커스텀 유저 디테일 작성
    2. 유저 권한 설정
    3. 뷰단 페이지 수정

--- antico/src/main/java/com/project/app/member/service/MemberService_imple.java
+++ antico/src/main/java/com/project/app/member/service/MemberService_imple.java
@@ -1,6 +1,7 @@
 package com.project.app.member.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,8 +17,13 @@ public class MemberService_imple implements MemberService {
 	@Autowired
 	private MemberDAO memberDAO;
 	
+	@Autowired
+	private PasswordEncoder pwdEncoder;
+	
 	@Override
 	public int registerMember(MemberVO mvo) {
+		mvo.setMem_passwd(pwdEncoder.encode(mvo.getMem_passwd()));
+		
 		return memberDAO.registerMember(mvo);
 	}
 
