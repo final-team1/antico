7358b1e9b2ce5353be57490ebbf4501c7c28e642
HalftheWorldAway <qhdb18@naver.com>
2025-02-14 18:03:24 +0900

    [chore] : 상품 등록 페이지 이미지 미리보기 업데이트
    1. 이미지 미리보기 업데이트
    2. 테이블 변경에 따른 상품/상품이미지/카테고리상위/카테고리하위 VO 업데이트
    3.mybatis-config.xml 상품 관련 VO 추가

--- antico/src/main/java/com/project/app/product/service/ProductService.java
+++ antico/src/main/java/com/project/app/product/service/ProductService.java
@@ -1,5 +1,16 @@
 package com.project.app.product.service;
 
+import java.util.List;
+
+import com.project.app.product.domain.CategoryDetailVO;
+import com.project.app.product.domain.CategoryVO;
+
 public interface ProductService {
+	
+	// 상품등록 form 페이지에 상위 카테고리명 보여주기
+	List<CategoryVO> getCategory();
+	
+	// 상품등록 form 페이지에 하위 카테고리명 보여주기
+	List<CategoryDetailVO> getCategoryDetail();
 
 }
