6a8ef1ca9d4913e65b174b0808443165c21dee77
jangduck <max5752@naver.com>
2025-02-18 23:58:17 +0900

    style: 계좌관리 view
    계좌관리 view 페이지 제작 완료.
    변경버튼 클릭이벤트(ajax)와 정산내역 클릭이벤트(ajax)부터 시작하면 됨

--- antico/src/main/java/com/project/app/mypage/service/MypageService.java
+++ antico/src/main/java/com/project/app/mypage/service/MypageService.java
@@ -1,14 +1,11 @@
 package com.project.app.mypage.service;
 
-import com.project.app.member.domain.MemberVO;
-import com.project.app.mypage.domain.LoginHistoryVO;
+import com.project.app.mypage.domain.LeaveVO;
 
 public interface MypageService {
+    // 탈퇴 신청 로직
+    int deletesubmit(LeaveVO lvo);
 
-	// 탈퇴 처리 로직
-	int deletesubmit(MemberVO mvo, LoginHistoryVO lhvo);
-
-	 // 결제하기를 눌렀을 경우 해당 포인트 업데이트
-//	int pointcharge();
-
+    // 로그인 시 탈퇴 신청 확인 및 취소 로직
+    int cancelLeaveRequest(int memberNo);
 }
