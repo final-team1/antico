881524ee4de755208aaec5836ef545c74946a837
jangduck <max5752@naver.com>
2025-03-05 23:43:12 +0900

    fix: 탈퇴하기 기능에러 수정
    72시간 체크 업데이트가 안 되고 있어서 전체적인 틀을 바꿔서 수정했습니다.
    현재 정상적으로 동작

--- antico/src/main/java/com/project/app/member/model/MemberDAO.java
+++ antico/src/main/java/com/project/app/member/model/MemberDAO.java
@@ -16,13 +16,10 @@ public interface MemberDAO {
 	MemberVO selectMemberByMemberNo(String memberNo);
 
 	// 탈퇴신청을 한 회원인지 체크
-	String leaveCheck(String member_user_id);
+	String leaveCheck(String pk_member_no);
 
 	// 로그인시 탈퇴신청 후 72시간이 지난 회원이 있는지 조회
 	String loginCheck(String member_user_id);
-
-	// 72시간이 지난 회원이 로그인을 할 때 회원상태 업데이트
-	void statusUpdate(String fk_member_no);
 
 	// 탈퇴신청은 했지만 72시간이 지나지 않은 회원이 로그인한 경우
 	void leaveDelete(String leave_member_no);
@@ -38,5 +35,10 @@ public interface MemberDAO {
 
 	// 로그인시 로그인 기록을 남기는 메소드
 	void loginHistoryByLoginHistoryVo(LoginHistoryVO login_history_vo);
+	
+	 // 탈퇴신청은 했지만 72시간 이내에 접속한 경우
+	void rollback(String leave_member_no);
+
+
 
 }
--- antico/src/main/java/com/project/app/member/service/MemberDetailService.java
+++ antico/src/main/java/com/project/app/member/service/MemberDetailService.java
@@ -3,15 +3,12 @@ package com.project.app.member.service;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,12 +25,12 @@ import lombok.AllArgsConstructor;
 @Service
 public class MemberDetailService implements UserDetailsService{
 
-	@Autowired
-	private MemberDAO member_dao;
-	
-	@Autowired
-	private MemberVO member_vo;
-	
+   @Autowired
+   private MemberDAO member_dao;
+   
+   @Autowired
+   private MemberVO member_vo;
+   
     @Autowired
     private HttpServletResponse response;
     
@@ -46,7 +43,6 @@ public class MemberDetailService implements UserDetailsService{
 	@Override
 	public UserDetails loadUserByUsername(String member_user_id) throws UsernameNotFoundException {
 		
-		
 		member_vo = member_dao.selectMemberByUserId(member_user_id);
 		// TODO memberVO 유효성검사
 		
@@ -55,105 +51,79 @@ public class MemberDetailService implements UserDetailsService{
 		String ctx_path = request.getContextPath();
 		
 		// 탈퇴신청을 한 회원인지 체크
-		String leave_member_no = member_dao.leaveCheck(member_user_id);
-		
+		String leave_member_no = member_dao.leaveCheck(member_vo.getPk_member_no());
 		
 		Cookie cookie;
 
 		// 로그인시 회원상태가 1인것만 로그인 가능하도록
-		String member_status = member_vo.getMember_status();
-		
-		if("0".equals(member_status)) {
-			try {
-				cookie = new Cookie("message", URLEncoder.encode("이미&nbsp;탈퇴한&nbsp;회원입니다.", "UTF-8"));
-				
-				cookie.setMaxAge(5); 
-				
-				cookie.setPath("/");
-				
-				response.addCookie(cookie);
-				
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-			
-			try {
-				response.sendRedirect(ctx_path+"/logout");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			
-			if(leave_member_no != null) { // 탈퇴신청을 한 회원이라면
-				// 로그인시 탈퇴신청 후 72시간이 지난 회원이 있는지 조회
-				String fk_member_no = member_dao.loginCheck(leave_member_no);
-			//	System.out.println("로그인시 탈퇴 신청후 72시간이 지난 회원이 있는지 확인"+fk_member_no);
-				
-				if(fk_member_no != null) { // 탈퇴신청을 한 회원이면서 72시간이 지났다면
-					// 72시간이 지난 회원이 로그인을 할 때 회원상태 업데이트
-					member_dao.statusUpdate(fk_member_no);
+		String fk_member_no = "";
+		if(leave_member_no != null) { // 탈퇴신청한 회원이 존재한다면
+			fk_member_no = member_dao.loginCheck(leave_member_no); // 로그인시 탈퇴신청 후 72시간이 지난 회원이 있는지 조회
+			System.out.println("탈퇴신청을 했지만 72시간이 지났는지 조회"+ fk_member_no);
+			if(fk_member_no == null) { // 탈퇴는 했지만 72시간이 지나지 않은 회원이 로그인을 했다면
+				member_dao.leaveDelete(leave_member_no); // 탈퇴테이블에서 값을 지우기
+				member_dao.rollback(leave_member_no); // 상태를 다시 돌려놓기
+				try {
+					cookie = new Cookie("message", URLEncoder.encode("탈퇴&nbsp;신청이&nbsp;취소되었습니다.", "UTF-8"));
+					cookie.setMaxAge(5);
+
+					cookie.setPath("/");
+
+					response.addCookie(cookie);
+
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+			} else { // 탈퇴를 했으면서 72시간이 지난 회원이 로그인을 했더라면
+				try {
+					cookie = new Cookie("message", URLEncoder.encode("탈퇴&nbsp;후&nbsp;72시간이&nbsp;지나&nbsp;로그인이&nbsp;불가능합니다.", "UTF-8"));
 					
-					try {
-						cookie = new Cookie("message", URLEncoder.encode("탈퇴&nbsp;후&nbsp;72시간이&nbsp;지나&nbsp;로그인이&nbsp;불가능합니다.", "UTF-8"));
-						
-						cookie.setMaxAge(5); 
-						
-						cookie.setPath("/");
-						
-						response.addCookie(cookie);
-						
-					} catch (UnsupportedEncodingException e) {
-						e.printStackTrace();
-					}
+					cookie.setMaxAge(5); 
 					
+					cookie.setPath("/");
+					
+					response.addCookie(cookie);
 					try {
 						response.sendRedirect(ctx_path+"/logout");
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-				} else {
-					// 탈퇴신청은 했지만 72시간이 지나지 않은 회원이 로그인한 경우
-					member_dao.leaveDelete(leave_member_no);
-					
-					try {
-						cookie = new Cookie("message", URLEncoder.encode("탈퇴&nbsp;신청이&nbsp;취소되었습니다.", "UTF-8"));
-						
-						cookie.setMaxAge(5); 
-						
-						cookie.setPath("/");	
-						
-						response.addCookie(cookie);
-						
-					} catch (UnsupportedEncodingException e) {
-						e.printStackTrace();
-					}
-					try {
-						response.sendRedirect(ctx_path+"/");
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
 				}
+				
+				try {
+					response.sendRedirect(ctx_path+"/logout");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} 
+		else { // 탈퇴를 하지 않았을 경우
+			try {
+				response.sendRedirect(ctx_path + "/");
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 		return User.withUserDetails(user_detail).build();		
 	}
 
 
-
-	
-/*	public static UserBuilder withUserDetails(UserDetails userDetails) {
-		// @formatter:off
-		return withUsername(userDetails.getUsername())
-				.password(userDetails.getPassword())
-				.accountExpired(!userDetails.isAccountNonExpired())
-				.accountLocked(!userDetails.isAccountNonLocked())
-				.authorities(userDetails.getAuthorities())
-				.credentialsExpired(!userDetails.isCredentialsNonExpired())
-				.disabled(!userDetails.isEnabled());
-		// @formatter:on
-	}*/
-	
-	
+   
+/*   public static UserBuilder withUserDetails(UserDetails userDetails) {
+      // @formatter:off
+      return withUsername(userDetails.getUsername())
+            .password(userDetails.getPassword())
+            .accountExpired(!userDetails.isAccountNonExpired())
+            .accountLocked(!userDetails.isAccountNonLocked())
+            .authorities(userDetails.getAuthorities())
+            .credentialsExpired(!userDetails.isCredentialsNonExpired())
+            .disabled(!userDetails.isEnabled());
+      // @formatter:on
+   }*/
+   
+   
 }
 
 
