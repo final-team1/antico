dfa41f6f5f86ad9d02b4bac5ca7ae4d27a90863b
jangduck <max5752@naver.com>
2025-03-06 15:49:00 +0900

    fix: 탈퇴처리시 로그인
    sendredirect 중복 호출 제거 및 탈퇴한지 72시간이 지나 로그인이 불가능할 때 던진 계정만료 예외처리 완료

--- antico/src/main/java/com/project/app/member/service/MemberDetailService.java
+++ antico/src/main/java/com/project/app/member/service/MemberDetailService.java
@@ -1,10 +1,8 @@
 package com.project.app.member.service;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,102 +14,51 @@ import com.project.app.member.domain.MemberVO;
 import com.project.app.member.model.MemberDAO;
 import com.project.app.security.CustomUserDetails;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @AllArgsConstructor
 @Service
-public class MemberDetailService implements UserDetailsService{
+public class MemberDetailService implements UserDetailsService {
 
-   @Autowired
-   private MemberDAO member_dao;
-   
-   @Autowired
-   private MemberVO member_vo;
-   
-    @Autowired
-    private HttpServletResponse response;
-    
-    @Autowired
-    private HttpServletRequest request;
+	@Autowired
+	private MemberDAO member_dao;
 
+	@Autowired
+	private MemberVO member_vo;
 
-
-    @Transactional
+	@Transactional
 	@Override
-	public UserDetails loadUserByUsername(String member_user_id) throws UsernameNotFoundException {
-		
+	public UserDetails loadUserByUsername(String member_user_id) throws UsernameNotFoundException, AccountExpiredException {
+
 		member_vo = member_dao.selectMemberByUserId(member_user_id);
 		// TODO memberVO 유효성검사
-		
+
 		CustomUserDetails user_detail = new CustomUserDetails(member_vo);
-		
-		String ctx_path = request.getContextPath();
-		
+
 		// 탈퇴신청을 한 회원인지 체크
 		String leave_member_no = member_dao.leaveCheck(member_vo.getPk_member_no());
-		
-		Cookie cookie;
 
-		// 로그인시 회원상태가 1인것만 로그인 가능하도록
-		String fk_member_no = "";
-		if(leave_member_no != null) { // 탈퇴신청한 회원이 존재한다면
-			fk_member_no = member_dao.loginCheck(leave_member_no); // 로그인시 탈퇴신청 후 72시간이 지난 회원이 있는지 조회
-			System.out.println("탈퇴신청을 했지만 72시간이 지났는지 조회"+ fk_member_no);
-			if(fk_member_no == null) { // 탈퇴는 했지만 72시간이 지나지 않은 회원이 로그인을 했다면
+		if (leave_member_no != null) { // 탈퇴신청한 회원이 존재한다면
+
+			String fk_member_no = member_dao.loginCheck(leave_member_no); // 로그인시 탈퇴신청 후 72시간이 지난 회원이 있는지 조회
+
+			log.warn("탈퇴신청을 했지만 72시간이 지났는지 조회" + fk_member_no);
+
+			if (fk_member_no == null) { // 탈퇴는 했지만 72시간이 지나지 않은 회원이 로그인을 했다면
+
 				member_dao.leaveDelete(leave_member_no); // 탈퇴테이블에서 값을 지우기
+
 				member_dao.rollback(leave_member_no); // 상태를 다시 돌려놓기
-				try {
-					cookie = new Cookie("message", URLEncoder.encode("탈퇴&nbsp;신청이&nbsp;취소되었습니다.", "UTF-8"));
-					cookie.setMaxAge(5);
-
-					cookie.setPath("/");
-
-					response.addCookie(cookie);
-
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				}
 			} else { // 탈퇴를 했으면서 72시간이 지난 회원이 로그인을 했더라면
-				try {
-					cookie = new Cookie("message", URLEncoder.encode("탈퇴&nbsp;후&nbsp;72시간이&nbsp;지나&nbsp;로그인이&nbsp;불가능합니다.", "UTF-8"));
-					
-					cookie.setMaxAge(5); 
-					
-					cookie.setPath("/");
-					
-					response.addCookie(cookie);
-					try {
-						response.sendRedirect(ctx_path+"/logout");
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				}
-				
-				try {
-					response.sendRedirect(ctx_path+"/logout");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		} 
-		else { // 탈퇴를 하지 않았을 경우
-			try {
-				response.sendRedirect(ctx_path + "/");
-			} catch (IOException e) {
-				e.printStackTrace();
+				throw new AccountExpiredException("탈퇴&nbsp;후&nbsp;72시간이&nbsp;지나&nbsp;로그인이&nbsp;불가능합니다.");
 			}
 		}
-		return User.withUserDetails(user_detail).build();		
+		return User.withUserDetails(user_detail).build();
 	}
 
-
-   
-/*   public static UserBuilder withUserDetails(UserDetails userDetails) {
+	/*   public static UserBuilder withUserDetails(UserDetails userDetails) {
       // @formatter:off
       return withUsername(userDetails.getUsername())
             .password(userDetails.getPassword())
@@ -122,10 +69,8 @@ public class MemberDetailService implements UserDetailsService{
             .disabled(!userDetails.isEnabled());
       // @formatter:on
    }*/
-   
-   
-}
 
+}
 
 /*
  * 
--- antico/src/main/java/com/project/app/security/LoginFailureHandler.java
+++ antico/src/main/java/com/project/app/security/LoginFailureHandler.java
@@ -1,8 +1,11 @@
 package com.project.app.security;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+
 import java.io.IOException;
 import java.net.URLEncoder;
 
+import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -16,12 +19,23 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LoginFailureHandler implements AuthenticationFailureHandler {
 
 	@Override
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException e) throws IOException, ServletException {
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+		
+		String message = "";
+		
+		// 탈퇴한지 72시간이 지나 로그인이 불가능할 때 던진 계정만료 예외
+		// TODO InternalAuthenticationServiceException로 래핑된 예외인데 이 예외도 체크해야 하는지 확인
+		if(e.getCause() instanceof AccountExpiredException) {
+			message = e.getMessage();
+		}
+		else {
+			e.printStackTrace();
+			message = "로그인에&nbsp;실패했습니다.&nbsp;아이디,&nbsp;비밀번호를&nbsp;확인해주세요.";
+		}
 		
 		String ctx_path = request.getContextPath();
 		
-		Cookie cookie = new Cookie("message", URLEncoder.encode("로그인에&nbsp;실패했습니다.&nbsp;아이디,&nbsp;비밀번호를&nbsp;확인해주세요.", "UTF-8"));
+		Cookie cookie = new Cookie("message", URLEncoder.encode(message, "UTF-8"));
 		
 		cookie.setMaxAge(5); 
 		
